350318d4649a043302fe64d7ff2b1a06126ad11e
Ubuntu <ubuntu@ip-172-31-24-20.us-west-1.compute.internal>
2024-02-29 00:04:10 +0000

    linked pages

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 #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
 from flask import Flask, render_template
+from datetime import date
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    today = todays_date()
+    return render_template("index.html", the_date = today)
     
 @app.route("/about")
 def about():
     return render_template("about_me.html")
+
+
+def todays_date():
+    today = date.today()
+    str_date = today.strftime("%B %d, %Y")
+    return "Today's date is " + str_date
